aca3948f2b708e8d2f4f83dc82c793501b123b2d
Ernesto Baschny <ernst@cron-it.de>
2011-10-31 13:21:25 +0100

    Also move css file to remote server

--- update-changes.sh
+++ update-changes.sh
@@ -2,4 +2,4 @@
 
 base=/home/ernst/TYPO3-Release
 php $base/check-changes.php
-rsync -a $base/index.html spinat.serverdienst.net:/www/sites/web77000/html/typo3-merges/
+rsync -a $base/index.html $base/styles.css spinat.serverdienst.net:/www/sites/web77000/html/typo3-merges/
