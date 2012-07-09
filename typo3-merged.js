21f6278b630bd3d73a2e293de772216b9de0b395
Mario Rimann <mario@rimann.org>
2012-07-09 23:00:47 +0200

    added filtering-out the entries that are done
    only the entries that need some action (e.g. back porting a patch) are shown, these are all those entries with a "TODO"

--- typo3-merged.js
+++ typo3-merged.js
@@ -1,18 +1,31 @@
 $(document).ready(function(){
+	// add button to control filtering
+	$('h2').after('<div id="controls"></div>');
 
-    $('.info-planned').each(function() {
-        issue = $(this).attr('issue');
-        branch = $(this).attr('branch');
-        //alert(issue +  branch);
-    });
 
-    data = {jsonrpc:"2.0",method:"allQueryNext",params:["tr:38604 branch:TYPO3_4-7","z",25],"id":5};
-    data = JSON.stringify(data);
-    $.post({
-        url: 'https://review.typo3.org/gerrit/rpc/ChangeListService',
-        data: data,
-        success: function() {},
-        dataType: 'json'
-    });
+	$('#controls').append('<button id="showAllEntries">show all</button>');
+	$('#showAllEntries').click(function() {
+		$('tr').show();
+	});
+
+	$('#controls').append('<button id="hideResolved">hide done</button>');
+	$('#hideResolved').click(function() {
+		hideResolved();
+	});
+
+	/**
+	 * Hides all rows that have not a single TODO (e.g. those that need no
+	 * action at the moment)
+	 */
+	function hideResolved() {
+		// hide all those entries where nothing has to be done
+		$('tr').addClass('nothingToDo');
+
+		$('.info-planned').each(function() {
+			$(this).parent().removeClass('nothingToDo');
+		});
+		$('tr.nothingToDo').hide();
+		$('tbody tr:first').show();
+	}
 
 });
