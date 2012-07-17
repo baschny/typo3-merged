1d8f60b72a58edfa57a7873de79e64a485cc1a33
Ernesto Baschny <ernst@cron-it.de>
2012-07-17 16:52:00 +0200

    [TASK] Added configuration for TYPO3 version/workspaces sysext

---
+++ typo3-workspaces.php
@@ -0,0 +1,51 @@
+<?php
+// configuration for TYPO3 system extensions:
+// - version
+// - workspaces
+
+$gitRoot = '/www/shared/TYPO3ext/';
+$gitRootIsWorkingCopy = TRUE;
+$htmlFile = 'workspaces.html';
+
+$reviewLinkPattern = "https://review.typo3.org/#/q/tr:%s,n,z";
+
+$issueMapping = array(
+);
+
+$projectsToCheck = array(
+	'TYPO3 Version' => array(
+		'gitWebUrl' => 'http://git.typo3.org/TYPO3v4/CoreProjects/workspaces/version.git',
+		'releases' => array(
+			# project, starting point, branch, working copy path
+			array('4.5', 'refs/tags/4.5.0', 'origin/4.5', 'version/4.5'),
+			array('4.6', 'refs/tags/4.5.0', 'origin/4.6', 'version/4.6'),
+			array('4.7', 'refs/tags/4.5.0', 'origin/version_4-7', 'version/4.7'),
+			array('6.0', 'refs/tags/4.5.0', 'origin/master', 'version/master'),
+		),
+	),
+	'TYPO3 Workspaces' => array(
+		'gitWebUrl' => 'http://git.typo3.org/TYPO3v4/CoreProjects/workspaces/workspaces.git',
+		'releases' => array(
+			# project, starting point, branch, working copy path
+			array('4.5', 'refs/tags/4.5.0', 'origin/4.5', 'workspaces/4.5'),
+			array('4.6', 'refs/tags/4.5.0', 'origin/4.6', 'workspaces/4.6'),
+			array('4.7', 'refs/tags/4.5.0', 'origin/workspaces_4-7', 'workspaces/4.7'),
+			array('6.0', 'refs/tags/4.5.0', 'origin/master', 'workspaces/master'),
+		),
+	),
+);
+
+/**
+ * Callback to detect if this commit is a "release" commit
+ *
+ * @param $commitInfos array The infos from the commit
+ * @return mixed FALSE|string The released version name
+ */
+function getDetectedReleaseCommitCallback($commitInfos) {
+	if (preg_match('/TYPO3_([0-9-]{5}(?:-(alpha|beta|rc)[0-9]+)?)/', $commitInfos['tags'], $matches)) {
+		return $matches[1];
+	}
+	return NULL;
+}
+
+?>
