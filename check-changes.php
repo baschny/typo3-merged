e5decbeea70e02eb6484b30c420c2ae9533635cf
Ernesto Baschny <ernst@cron-it.de>
2012-08-19 17:55:42 +0200

    Skip issues marked as done in "todo" filtering
    Do not add "info-planned" class for those entries so they won't add
    up in the filters for TODOs.

--- check-changes.php
+++ check-changes.php
@@ -355,7 +355,7 @@ foreach ($projectsToCheck as $project => $projectData) {
 				}
 			} elseif (isset($projectData['ignoreList'][$branchName][$topic])) {
 				// in case this issue + branch combination are on the ignore list, mark it appropriately
-				$class = 'info-planned info-not-needed';
+				$class = 'info-not-needed';
 				$text = sprintf('<span title="%s" href="" target="_blank">given up</span>',
 					$projectData['ignoreList'][$branchName][$topic]
 				);
