442ec6dc2ff7969cc38b9a7decc19edb769bba7c
Stephan Großberndt <s.grossberndt@sidebysite.de>
2015-06-29 21:53:21 +0200

    Added issue #66895 to the TYPO3_6-2 ignoreList

--- typo3-core.php
+++ typo3-core.php
@@ -103,6 +103,7 @@ $projectsToCheck = array(
 				'64618' => 'Patch is not relevant for 6.2 (found out after merging)',
 				'66167' => 'No backport possible, code not present in 6.2',
 				'66729' => 'No backport needed as the feature (Gerrit #30972) wasn\'t backported either.',
+				'66895' => 'Abandoned for 6.2, see comment in Gerrit #39463',
 				'67274' => 'Will not be backported to 6.2, see comment in Gerrit #40056'
 			)
 		),
