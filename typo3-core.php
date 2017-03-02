1d832ecb2f278b4a06dbfefd1a557492a98fb078
Stephan Großberndt <s.grossberndt@sidebysite.de>
2017-03-02 20:57:12 +0100

    Added 79406 and 80014 to 7.6 ignorelist

--- typo3-core.php
+++ typo3-core.php
@@ -74,6 +74,8 @@ $projectsToCheck = array(
 				'77248' => 'Already fixed with #49185',
 				'78900' => 'Not relevant for 7.6, see comment in Gerrit #50931',
 				'79006' => 'Not relevant for 7.6, see comment in Gerrit #50977',
+				'79406' => 'Not relevant for 7.6 (found out after merging #51379)',
+				'80014' => 'Not required for 7.6, see comment in Gerrit #51844',
 			),
 			'master' => array(
 				'65224' => 'In master with #35997 and #36501, #37111 was only a backport of those to 6.2',
